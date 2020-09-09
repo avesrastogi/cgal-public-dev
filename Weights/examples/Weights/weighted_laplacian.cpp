1b8418290711641a7b243afa1df12a068515ffa2
Dmitry Anisimov <danston@ymail.com>
2020-09-09 11:34:09 +0200

    updated example

--- Weights/examples/Weights/weighted_laplacian.cpp
+++ Weights/examples/Weights/weighted_laplacian.cpp
@@ -19,79 +19,75 @@ using Mesh = CGAL::Surface_mesh<Point_3>;
 using VD   = boost::graph_traits<Mesh>::vertex_descriptor;
 using HD   = boost::graph_traits<Mesh>::halfedge_descriptor;
 
-struct Weight_wrapper {
+template<typename PointMap>
+FT get_w_ij(
+  const Mesh& mesh, const HD he, const PointMap pmap) {
 
-  template<typename PointMap>
-  FT w_ij(
-    const Mesh& mesh, const HD he, const PointMap pmap) const {
+  const auto v0 = target(he, mesh);
+  const auto v1 = source(he, mesh);
 
-    const auto v0 = target(he, mesh);
-    const auto v1 = source(he, mesh);
+  const auto& q  = get(pmap, v0); // query
+  const auto& p1 = get(pmap, v1); // neighbor j
 
-    const auto& q  = get(pmap, v0); // query
-    const auto& p1 = get(pmap, v1); // neighbor j
-
-    if (is_border_edge(he, mesh)) {
-      const auto he_cw = opposite(next(he, mesh), mesh);
-      auto v2 = source(he_cw, mesh);
-
-      if (is_border_edge(he_cw, mesh)) {
-        const auto he_ccw = prev(opposite(he, mesh), mesh);
-        v2 = source(he_ccw, mesh);
-
-        const auto& p2 = get(pmap, v2); // neighbor jp
-        return CGAL::Weights::cotangent(p1, p2, q);
-      } else {
-        const auto& p0 = get(pmap, v2); // neighbor jm
-        return CGAL::Weights::cotangent(q, p0, p1);
-      }
-    }
-
+  if (is_border_edge(he, mesh)) {
     const auto he_cw = opposite(next(he, mesh), mesh);
-    const auto v2 = source(he_cw, mesh);
-    const auto he_ccw = prev(opposite(he, mesh), mesh);
-    const auto v3 = source(he_ccw, mesh);
+    auto v2 = source(he_cw, mesh);
 
-    const auto& p0 = get(pmap, v2); // neighbor jm
-    const auto& p2 = get(pmap, v3); // neighbor jp
-    return CGAL::Weights::cotangent_weight(p0, p1, p2, q) / FT(2);
-  }
+    if (is_border_edge(he_cw, mesh)) {
+      const auto he_ccw = prev(opposite(he, mesh), mesh);
+      v2 = source(he_ccw, mesh);
 
-  template<typename PointMap>
-  FT w_i(
-    const Mesh& mesh, const VD v_i, const PointMap pmap) const {
-
-    FT A_i = FT(0);
-    const auto v0 = v_i;
-    const auto init = halfedge(v_i, mesh);
-    for (const auto he : halfedges_around_target(init, mesh)) {
-      assert(v0 == target(he, mesh));
-      if (is_border(he, mesh)) { continue; }
-
-      const auto v1 = source(he, mesh);
-      const auto v2 = target(next(he, mesh), mesh);
-
-      const auto& p = get(pmap, v0);
-      const auto& q = get(pmap, v1);
-      const auto& r = get(pmap, v2);
-      A_i += CGAL::Weights::mixed_voronoi_area(p, q, r);
+      const auto& p2 = get(pmap, v2); // neighbor jp
+      return CGAL::Weights::cotangent(p1, p2, q);
+    } else {
+      const auto& p0 = get(pmap, v2); // neighbor jm
+      return CGAL::Weights::cotangent(q, p0, p1);
     }
-    assert(A_i != FT(0));
-    return FT(1) / (FT(2) * A_i);
   }
-};
+
+  const auto he_cw = opposite(next(he, mesh), mesh);
+  const auto v2 = source(he_cw, mesh);
+  const auto he_ccw = prev(opposite(he, mesh), mesh);
+  const auto v3 = source(he_ccw, mesh);
+
+  const auto& p0 = get(pmap, v2); // neighbor jm
+  const auto& p2 = get(pmap, v3); // neighbor jp
+  return CGAL::Weights::cotangent_weight(p0, p1, p2, q) / FT(2);
+}
+
+template<typename PointMap>
+FT get_w_i(
+  const Mesh& mesh, const VD v_i, const PointMap pmap) {
+
+  FT A_i = FT(0);
+  const auto v0 = v_i;
+  const auto init = halfedge(v_i, mesh);
+  for (const auto he : halfedges_around_target(init, mesh)) {
+    assert(v0 == target(he, mesh));
+    if (is_border(he, mesh)) { continue; }
+
+    const auto v1 = source(he, mesh);
+    const auto v2 = target(next(he, mesh), mesh);
+
+    const auto& p = get(pmap, v0);
+    const auto& q = get(pmap, v1);
+    const auto& r = get(pmap, v2);
+    A_i += CGAL::Weights::mixed_voronoi_area(p, q, r);
+  }
+  assert(A_i != FT(0));
+  return FT(1) / (FT(2) * A_i);
+}
 
 void set_laplacian_matrix(
   const Mesh& mesh, Matrix& L) {
 
-  const Weight_wrapper wrapper;
   const auto pmap = get(CGAL::vertex_point, mesh); // vertex to point map
   const auto imap = get(CGAL::vertex_index, mesh); // vertex to index map
 
   // Precompute Voronoi areas.
   std::map<std::size_t, FT> w_i;
   for (const auto v_i : vertices(mesh)) {
-    w_i[get(imap, v_i)] = wrapper.w_i(mesh, v_i, pmap);
+    w_i[get(imap, v_i)] = get_w_i(mesh, v_i, pmap);
   }
 
   // Fill the matrix.
@@ -103,7 +99,7 @@ void set_laplacian_matrix(
     const std::size_t j = get(imap, vj);
     if (i > j) { continue; }
 
-    const FT w_ij = w_i[j] * wrapper.w_ij(mesh, he, pmap);
+    const FT w_ij = w_i[j] * get_w_ij(mesh, he, pmap);
     L.set_coef(i, j,  w_ij, true);
     L.set_coef(j, i,  w_ij, true);
     L.add_coef(i, i, -w_ij);
